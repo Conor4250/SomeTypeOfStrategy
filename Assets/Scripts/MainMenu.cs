df3928dbeefa165594113c1a03d6c8e007566b50
Conor4250 <1810150+Conor4250@users.noreply.github.com>
2020-03-11 13:33:30 +0000

    Recovered WIP Main Menu
    Got deleted by accident

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//I've messed up somewhere either in unity or in the script. It plays all 3 buttons that have been pressed on one button click.
+public class MainMenu : MonoBehaviour {
+    public bool Start;
+    public bool Help;
+    public bool Quit;
+    public bool StartEditor;
+    void Update () {
+        if (Input.GetMouseButtonUp (0)) {
+            if (Start) {
+                SceneManager.LoadScene ("PrototypeScene");
+                Debug.Log ("Start clicked");
+            }
+            if (StartEditor) {
+                EditorSceneManager.OpenScene ("Assets/Scenes/PrototypeScene.unity");
+                Debug.Log ("Start Editor clicked");
+            }
+            if (Help) {
+                //Coming soon
+                Debug.Log ("Help clicked");
+            }
+            if (Quit) {
+                Application.Quit ();
+                Debug.Log ("Quit clicked");
+            }
+        }
+    }
+
+}
