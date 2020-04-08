fe452b2736b430062d24dd631e245e4b628ec0cd
Conor4250 <1810150+Conor4250@users.noreply.github.com>
2020-04-08 17:29:52 +0100

    Few changes
    Been looking into the spawning not working
    Changed the main menu so it keyboard operated instead of mouse click

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
 //I've messed up somewhere either in unity or in the script. It plays all 3 buttons that have been pressed on one button click.
 public class MainMenu : MonoBehaviour {
     public bool Start;
@@ -11,24 +13,39 @@ public class MainMenu : MonoBehaviour {
     public bool Quit;
     public bool StartEditor;
     void Update () {
-        if (Input.GetMouseButtonUp (0)) {
-            if (Start) {
-                SceneManager.LoadScene ("PrototypeScene");
-                Debug.Log ("Start clicked");
-            }
-            if (StartEditor) {
-                EditorSceneManager.OpenScene ("Assets/Scenes/PrototypeScene.unity");
-                Debug.Log ("Start Editor clicked");
-            }
-            if (Help) {
-                //Coming soon
-                Debug.Log ("Help clicked");
-            }
-            if (Quit) {
-                Application.Quit ();
-                Debug.Log ("Quit clicked");
-            }
+        //if (Input.GetMouseButtonUp (0)) {
+        //    if (Start) {
+        //        SceneManager.LoadScene ("PrototypeScene");
+        //        Debug.Log ("Start clicked");
+        //    }
+        //    if (StartEditor) {
+        //        EditorSceneManager.OpenScene ("Assets/Scenes/PrototypeScene.unity");
+        //        Debug.Log ("Start Editor clicked");
+        //    }
+        //    if (Help) {
+        //        //Coming soon
+        //        Debug.Log ("Help clicked");
+        //    }
+        //    if (Quit) {
+        //        Application.Quit ();
+        //        Debug.Log ("Quit clicked");
+        //    }
+        //}
+
+        if (Input.GetKeyUp("1"))
+        {
+            SceneManager.LoadScene("PrototypeScene");
+            Debug.Log("Start clicked");
+        } else if (Input.GetKeyUp("2"))
+        {
+            //Coming soon
+            Debug.Log("Help clicked");
+        } else if (Input.GetKeyUp("3"))
+        {
+            Application.Quit();
+            Debug.Log("Quit clicked");
         }
+
     }
 
 }
