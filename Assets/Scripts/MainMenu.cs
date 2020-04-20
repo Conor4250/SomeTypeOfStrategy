83942127ad12bef6ffee4326bb3103a195bd996c
Conor4250 <1810150+Conor4250@users.noreply.github.com>
2020-04-20 13:33:52 +0100

    Compiling issues fixed
    Also a bit of code cleanup and added the base for a help menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,35 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-#if UNITY_EDITOR
-using UnityEditor;
-using UnityEditor.SceneManagement;
-#endif
-//I've messed up somewhere either in unity or in the script. It plays all 3 buttons that have been pressed on one button click.
+
 public class MainMenu : MonoBehaviour {
-    public bool Start;
-    public bool Help;
-    public bool Quit;
-    public bool StartEditor;
     void Update () {
-        //if (Input.GetMouseButtonUp (0)) {
-        //    if (Start) {
-        //        SceneManager.LoadScene ("PrototypeScene");
-        //        Debug.Log ("Start clicked");
-        //    }
-        //    if (StartEditor) {
-        //        EditorSceneManager.OpenScene ("Assets/Scenes/PrototypeScene.unity");
-        //        Debug.Log ("Start Editor clicked");
-        //    }
-        //    if (Help) {
-        //        //Coming soon
-        //        Debug.Log ("Help clicked");
-        //    }
-        //    if (Quit) {
-        //        Application.Quit ();
-        //        Debug.Log ("Quit clicked");
-        //    }
-        //}
 
         if (Input.GetKeyUp("1"))
         {
